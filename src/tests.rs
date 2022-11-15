733781ebe32649c36b8f1255eea4206512dc67c6
polazarus <polazarus@users.noreply.github.com>
2022-11-15 13:51:49 +0100

    fix(tests): add miri-specific test
    disable proptest with miri

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,3 @@
-use alloc::format;
 use alloc::vec::Vec;
 use core::cell::RefCell;
 
@@ -15,15 +14,15 @@ impl<F: FnMut()> Drop for DropCallback<F> {
 
 #[test]
 fn test_drop() {
-    let mut witness = false;
+    let witness = Cell::new(false);
     {
         let rodeo = Rodeo::new();
         let _dcb = rodeo.alloc(DropCallback(|| {
-            witness = true;
+            witness.set(true);
         }));
-        assert!(!witness);
+        assert!(!witness.get());
     }
-    assert!(witness);
+    assert!(witness.get());
 }
 
 #[test]
@@ -42,41 +41,61 @@ fn test_no_drop() {
     let () = rodeo.alloc(());
 }
 
-proptest! {
+fn check_number_drop(n: u32) {
+    let witness = Cell::new(0);
 
-    #[test]
-    fn test_number_drop(n in 1..2000u32) {
-        let witness = Cell::new(0);
-
-        {
-            let rodeo = Rodeo::new();
-            for _ in 0..n {
-                let _ = rodeo.alloc(DropCallback(|| {
-                    witness.set(witness.get()+1);
-                }));
-            }
-            assert_eq!(witness.get(), 0);
+    {
+        let rodeo = Rodeo::new();
+        for _ in 0..n {
+            let _ = rodeo.alloc(DropCallback(|| {
+                witness.set(witness.get() + 1);
+            }));
         }
+        assert_eq!(witness.get(), 0);
+    }
 
-        assert_eq!(witness.get(), n);
+    assert_eq!(witness.get(), n);
+}
+
+fn check_order_drop(n: u8) {
+    let witness = RefCell::new(Vec::with_capacity(n as usize));
+
+    {
+        let rodeo = Rodeo::new();
+        for i in 0..n {
+            let witness = &witness;
+            let _ = rodeo.alloc(DropCallback(move || {
+                witness.borrow_mut().push(i);
+            }));
+        }
+        assert!(witness.borrow().is_empty());
+    }
+
+    let vec = witness.take();
+    assert_eq!(vec.len(), n as usize);
+    assert!(vec.windows(2).all(|w| w[0] >= w[1]));
+}
+
+#[test]
+fn test_number_drop_10() {
+    check_number_drop(10);
+}
+
+#[test]
+fn test_order_drop_10() {
+    check_order_drop(10);
+}
+
+proptest! {
+    #[test]
+    #[cfg_attr(miri, ignore)]
+    fn test_number_drop(n in 1..2000u32) {
+        check_number_drop(n);
     }
 
     #[test]
+    #[cfg_attr(miri, ignore)]
     fn test_order_drop(n in 2..100u8) {
-        let witness = RefCell::new(Vec::with_capacity(n as usize));
-
-        {
-            let rodeo = Rodeo::new();
-            for i in 0..n {
-                let _ = rodeo.alloc(DropCallback(|| {
-                    witness.borrow_mut().push(i);
-                }));
-            }
-            assert!(witness.borrow().is_empty());
-        }
-
-        let vec = witness.take();
-        prop_assert_eq!(vec.len(), n as usize);
-        prop_assert!(vec.windows(2).all(|w| w[0] >= w[1]));
+        check_order_drop(n);
     }
 }
